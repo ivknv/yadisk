d016c977c10d895e8c274bdbd4670cb662089deb
Ivan Konovalov <ivknv0@gmail.com>
2024-06-21 01:12:27 +0500

    Fix mypy errors in compat.py

--- yadisk/compat.py
+++ yadisk/compat.py
@@ -6,6 +6,13 @@ __all__ = ["List", "Dict", "Set", "Tuple", "Callable", "Iterable", "Generator",
 
 import sys
 
+from typing import Any
+
+List: Any
+Dict: Any
+Set: Any
+Tuple: Any
+
 if sys.version_info.major == 3 and sys.version_info.minor < 9:
     from typing import (
         List, Dict, Set, Tuple, Callable, Iterable, Generator, AsyncGenerator,
