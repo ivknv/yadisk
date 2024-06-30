a20a426fb7f5ca14c484936c77b76192660fabd1
Ivan Konovalov <ivknv0@gmail.com>
2024-06-30 23:50:13 +0500

    Update DiskInfoObject and UserObject

--- src/yadisk/objects/_disk.py
+++ src/yadisk/objects/_disk.py
@@ -18,9 +18,13 @@
 
 from functools import partial
 from ._yadisk_object import YaDiskObject
-from .._common import str_or_error, bool_or_error, int_or_error
+from .._typing_compat import Dict
+from .._common import str_or_error, bool_or_error, int_or_error, yandex_date
 
-from typing import Any, Optional, NoReturn
+from typing import Any, Optional, NoReturn, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import datetime
 
 __all__ = ["DiskInfoObject", "SystemFoldersObject", "UserObject", "UserPublicInfoObject"]
 
@@ -32,44 +36,54 @@ class DiskInfoObject(YaDiskObject):
         :param disk_info: `dict` or `None`
         :param yadisk: :any:`YaDisk` or `None`, `YaDisk` object
 
-        :ivar max_file_size: `int`, maximum supported file size (bytes)
         :ivar paid_max_file_size: `int`, maximum supported file size for a paid account (bytes)
-        :ivar unlimited_autoupload_enabled: `bool`, tells whether unlimited
-                                             autoupload from mobile devices is enabled
+        :ivar max_file_size: `int`, maximum supported file size (bytes)
         :ivar total_space: `int`, total disk size (bytes)
         :ivar trash_size: `int`, amount of space used by trash (bytes), part of `used_space`
-        :ivar is_paid: `bool`, tells if the account is paid or not
         :ivar used_space: `int`, amount of space used (bytes)
+        :ivar is_paid: `bool`, tells if the account is paid or not
+        :ivar is_idm_managed_folder_address_access: `bool`, not clear what this is for
+        :ivar reg_time: `datetime.datetime`, Disk registration date
         :ivar system_folders: :any:`SystemFoldersObject`, paths to the system folders
         :ivar user: :any:`UserObject`, owner of the disk
+        :ivar is_idm_managed_public_access: `bool`, not clear waht this is for
+        :ivar unlimited_autoupload_enabled: `bool`, tells whether unlimited
+                                             autoupload from mobile devices is enabled
         :ivar revision: `int`, current revision of Yandex.Disk
     """
 
-    max_file_size:                Optional[int]
-    paid_max_file_size:           Optional[int]
-    unlimited_autoupload_enabled: Optional[bool]
-    total_space:                  Optional[int]
-    trash_size:                   Optional[int]
-    is_paid:                      Optional[bool]
-    used_space:                   Optional[int]
-    system_folders:               "SystemFoldersObject"
-    user:                         "UserObject"
-    revision:                     Optional[int]
+    paid_max_file_size:                   Optional[int]
+    max_file_size:                        Optional[int]
+    total_space:                          Optional[int]
+    trash_size:                           Optional[int]
+    used_space:                           Optional[int]
+    is_paid:                              Optional[bool]
+    is_idm_managed_folder_address_access: Optional[bool]
+    reg_time:                             Optional["datetime.datetime"]
+    system_folders:                       "SystemFoldersObject"
+    user:                                 "UserObject"
+    is_idm_managed_public_access:         Optional[bool]
+    unlimited_autoupload_enabled:         Optional[bool]
+    revision:                             Optional[int]
 
-    def __init__(self, disk_info: Optional[dict] = None, yadisk: Optional[Any] = None):
+    def __init__(self, disk_info: Optional[Dict] = None, yadisk: Optional[Any] = None) -> None:
         YaDiskObject.__init__(
             self,
-            {"max_file_size":                int_or_error,
-             "paid_max_file_size":           int_or_error,
-             "unlimited_autoupload_enabled": bool_or_error,
-             "total_space":                  int_or_error,
-             "trash_size":                   int_or_error,
-             "is_paid":                      bool_or_error,
-             "used_space":                   int_or_error,
-             "system_folders":               partial(SystemFoldersObject, yadisk=yadisk),
-             "user":                         partial(UserObject, yadisk=yadisk),
-             "revision":                     int_or_error},
-            yadisk)
+            {"paid_max_file_size":                   int_or_error,
+             "max_file_size":                        int_or_error,
+             "total_space":                          int_or_error,
+             "trash_size":                           int_or_error,
+             "used_space":                           int_or_error,
+             "is_paid":                              bool_or_error,
+             "is_idm_managed_folder_address_access": bool_or_error,
+             "reg_time":                             yandex_date,
+             "system_folders":                       partial(SystemFoldersObject, yadisk=yadisk),
+             "user":                                 partial(UserObject, yadisk=yadisk),
+             "is_idm_managed_public_access":         bool_or_error,
+             "unlimited_autoupload_enabled":         bool_or_error,
+             "revision":                             int_or_error},
+            yadisk
+        )
 
         self.import_fields(disk_info)
 
@@ -114,9 +128,11 @@ class SystemFoldersObject(YaDiskObject):
     screenshots:   Optional[str]
     scans:         Optional[str]
 
-    def __init__(self,
-                 system_folders: Optional[dict] = None,
-                 yadisk: Optional[Any] = None):
+    def __init__(
+        self,
+        system_folders: Optional[Dict] = None,
+        yadisk: Optional[Any] = None
+    ) -> None:
         YaDiskObject.__init__(
             self,
             {"odnoklassniki": str_or_error,
@@ -146,25 +162,38 @@ class UserObject(YaDiskObject):
         :param user: `dict` or `None`
         :param yadisk: :any:`YaDisk` or `None`, `YaDisk` object
 
-        :ivar country: `str`, user's country
-        :ivar login: `str`, user's login
+        :ivar reg_time: `datetime.datetime`, Disk registration date
         :ivar display_name: `str`, user's display name
         :ivar uid: `str`, user's UID
+        :ivar country: `str`, user's country
+        :ivar is_child: `bool`, tells whether it's a child account
+        :ivar login: `str`, user's login
     """
 
-    country: Optional[str]
-    login: Optional[str]
+    reg_time:     Optional["datetime.datetime"]
     display_name: Optional[str]
-    uid: Optional[str]
+    uid:          Optional[str]
+    country:      Optional[str]
+    is_child:     Optional[bool]
+    login:        Optional[str]
 
-    def __init__(self, user: Optional[dict] = None, yadisk: Optional[Any] = None):
+    def __init__(
+        self,
+        user: Optional[Dict] = None,
+        yadisk: Optional[Any] = None
+    ) -> None:
         YaDiskObject.__init__(
             self,
-            {"country":      str_or_error,
-             "login":        str_or_error,
-             "display_name": str_or_error,
-             "uid":          str_or_error},
-            yadisk)
+            {
+                "reg_time":     yandex_date,
+                "display_name": str_or_error,
+                "uid":          str_or_error,
+                "country":      str_or_error,
+                "is_child":     bool_or_error,
+                "login":        str_or_error
+            },
+            yadisk
+        )
 
         self.import_fields(user)
 
@@ -184,9 +213,11 @@ class UserPublicInfoObject(UserObject):
 
     country: NoReturn  # pyright: ignore[reportIncompatibleVariableOverride]
 
-    def __init__(self,
-                 public_user_info: Optional[dict] = None,
-                 yadisk: Optional[Any] = None):
+    def __init__(
+        self,
+        public_user_info: Optional[Dict] = None,
+        yadisk: Optional[Any] = None
+    ) -> None:
         UserObject.__init__(self, None, yadisk)
         self.remove_field("country")
         self.import_fields(public_user_info)
