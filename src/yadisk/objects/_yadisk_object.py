4952e073874b88280534f7844017229f2719d697
Ivan Konovalov <ivknv0@gmail.com>
2025-06-17 15:59:46 +0500

    Override __dir__() method for API response objects

--- src/yadisk/objects/_yadisk_object.py
+++ src/yadisk/objects/_yadisk_object.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library; if not, see <http://www.gnu.org/licenses/>.
 
+from collections.abc import Generator
 from typing import Any, Optional
 from .._typing_compat import Callable, Iterator
 
@@ -49,6 +50,15 @@ class YaDiskObject:
         self.set_field_types(field_types)
 
         self._yadisk = yadisk
+
+    def __dir__(self) -> Generator[str, None, None]:
+        """
+            Return available attributes.
+        """
+
+        yield from super().__dir__()
+        yield from self.FIELD_TYPES.keys()
+        yield from self.ALIASES.keys()
 
     def set_field_types(self, field_types: dict) -> None:
         """
