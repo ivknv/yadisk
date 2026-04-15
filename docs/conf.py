65f1dce33136fab58e57a4cb9f796b333c3d4291
Ivan Konovalov <ivknv0@gmail.com>
2026-04-15 19:02:10 +0500

    chore: update intersphinx inventory link for requests

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx']
 
 intersphinx_mapping = {"python": ("https://docs.python.org/3", None),
-                       "requests": ("https://requests.readthedocs.io/en/master", None),
+                       "requests": ("https://requests.readthedocs.io/en/latest", None),
                        "aiohttp": ("https://docs.aiohttp.org/en/stable", None),
                        "pycurl": ("http://pycurl.io/docs/latest", None)}
 
